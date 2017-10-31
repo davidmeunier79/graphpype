d6ce35f85c582f32b22ca77fc8f480137c73f949
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2017-10-31 16:13:19 +0100

    mapflow_iterables for return_all_iter_cormats

--- graphpype/gather/gather_cormats.py
+++ graphpype/gather/gather_cormats.py
@@ -19,11 +19,11 @@ def isInAlphabeticalOrder(word):
     return list(word) == sorted(word)
     #return word==''.join(sorted(word))
 
-def return_all_iter_cormats(cormat_path ,iterables ,iternames, gm_mask_coords_file = 0, gm_mask_labels_file = 0,export_df = False):
+def return_all_iter_cormats(cormat_path ,iterables ,iternames, gm_mask_coords_file = 0, gm_mask_labels_file = 0,mapflow_iterables = 0, mapflow_iternames = 0,export_df = False):
     """
     gm_mask_coords_file is the coords commun to all analyses before Inteersectmask
     """
-    print(list(zip(*iterables)))
+    print(product(*iterables))
     
     all_iter_cormats = []
     all_descriptors = []
@@ -33,6 +33,8 @@ def return_all_iter_cormats(cormat_path ,iterables ,iternames, gm_mask_coords_fi
     if gm_mask_coords_file != 0:
         gm_mask_coords = np.loadtxt(gm_mask_coords_file)
     
+        print(gm_mask_coords)
+        
     if export_df:
         writer = pd.ExcelWriter(os.path.join(cormat_path,"all_cormats.xls"))
         
@@ -46,43 +48,97 @@ def return_all_iter_cormats(cormat_path ,iterables ,iternames, gm_mask_coords_fi
                             
         print(iter_dir)
         
-        cormat_file = os.path.join(cormat_path,iter_dir,"compute_conf_cor_mat","Z_cor_mat_resid_ts.npy")
-        
-        if os.path.exists(cormat_file):
-            cormat = np.load(cormat_file)
-            print(cormat.shape)
+        if mapflow_iterables == 0:
             
-            if gm_mask_coords_file != 0:
-                coords_file = os.path.join(cormat_path,iter_dir,"filter_ROI_mask_with_GM","filtered_coords_rois.txt")
-                coords = np.loadtxt(coords_file)
-            
-                cormat,_ = return_corres_correl_mat(cormat,coords,gm_mask_coords)
+            cormat_file = os.path.join(cormat_path,iter_dir,"compute_conf_cor_mat","Z_cor_mat_resid_ts.npy")
                 
-            if export_df:
-                if gm_mask_labels_file:
-                    labels = [line.strip() for line in open(gm_mask_labels_file)]
-                else: 
-                    labels = list(range(cormat.shape[0]))
+            if os.path.exists(cormat_file):
+                cormat = np.load(cormat_file)
+                print(cormat.shape)
+                
+                if gm_mask_coords_file != 0:
+                    coords_file = os.path.join(cormat_path,iter_dir,"extract_mean_ROI_ts","subj_coord_rois.txt")
+                    #coords_file = os.path.join(cormat_path,iter_dir,"filter_ROI_mask_with_GM","filtered_coords_rois.txt")
+                    coords = np.loadtxt(coords_file)
+                
+                    cormat,_ = return_corres_correl_mat(cormat,coords,gm_mask_coords)
                     
-                df = pd.DataFrame(cormat, columns = labels,index = labels)
-                print(df)
+                if export_df:
+                    if gm_mask_labels_file:
+                        labels = [line.strip() for line in open(gm_mask_labels_file)]
+                    else: 
+                        labels = list(range(cormat.shape[0]))
+                        
+                    df = pd.DataFrame(cormat, columns = labels,index = labels)
+                    print(df)
+                    
+                    df.to_excel(writer, "_".join(iter_obj))
+                    
+                print(cormat.shape)
                 
-                df.to_excel(writer, "_".join(iter_obj))
-                
-            all_iter_cormats.append(cormat)
-            all_descriptors.append(iter_obj)
+                all_iter_cormats.append(cormat)
+                all_descriptors.append(iter_obj)
+            
+            else:
+                print("Warning, file {}  could not be found".format(cormat_file))
             
         else:
-            print("Warning, file {}  could not be found".format(cormat_file))
-        
-    if export_df:
-        writer.save()
+            for i,map_iter in enumerate(mapflow_iterables):
+                
+                print(map_iter)
+                
+                cormat_file = os.path.join(cormat_path,iter_dir,"compute_conf_cor_mat","mapflow","_compute_conf_cor_mat"+str(i),"Z_cor_mat_resid_ts.npy")
+             
+                new_iter_obj = list(iter_obj)
+                
+                print(new_iter_obj)
+                
+                new_iter_obj.append(str(map_iter))
+                
+                print(new_iter_obj)
+                
+                if os.path.exists(cormat_file):
+                    cormat = np.load(cormat_file)
+                    print(cormat.shape)
+                    
+                    if gm_mask_coords_file != 0:
+                        coords_file = os.path.join(cormat_path,iter_dir,"extract_mean_ROI_ts","subj_coord_rois.txt")
+                        #coords_file = os.path.join(cormat_path,iter_dir,"filter_ROI_mask_with_GM","filtered_coords_rois.txt")
+                        coords = np.loadtxt(coords_file)
+                    
+                        cormat,_ = return_corres_correl_mat(cormat,coords,gm_mask_coords)
+                        
+                    if export_df:
+                        if gm_mask_labels_file:
+                            labels = [line.strip() for line in open(gm_mask_labels_file)]
+                        else: 
+                            labels = list(range(cormat.shape[0]))
+                            
+                        df = pd.DataFrame(cormat, columns = labels,index = labels)
+                        print(df)
+                        
+                        df.to_excel(writer, "_".join(new_iter_obj))
+                        
+                    print(cormat.shape)
+                    
+                    all_iter_cormats.append(cormat)
+                    all_descriptors.append(new_iter_obj)
+                    
+                else:
+                    print("Warning, file {}  could not be found".format(cormat_file))
+                
+    
+    #if export_df:
+        #writer.save()
         
     print(np.array(all_iter_cormats).shape)
     
+    if mapflow_iternames != 0:
+        iternames .append( mapflow_iternames)
     pd_all_descriptors = pd.DataFrame(all_descriptors,columns = iternames)
     
-    pd_all_descriptors.to_excel(os.path.join(cormat_path,"all_descriptors.xls"))
+    if export_df:
+        pd_all_descriptors.to_excel(os.path.join(cormat_path,"all_descriptors.xls"))
                             
     return np.array(all_iter_cormats),pd_all_descriptors
 
@@ -110,17 +166,17 @@ def compute_mean_cormats(all_cormats,all_descriptors,descript_columns):
         
             print(elem)
             #print lines
-            print(lines.index)
+            print((lines.index))
             
-            print(all_cormats.shape)
+            print((all_cormats.shape))
             
             elem_cormats = all_cormats[lines.index,:,:]
             
-            print(elem_cormats.shape)
+            print((elem_cormats.shape))
             
             mean_elem = np.mean(elem_cormats,axis = 0)
             
-            print(mean_elem.shape)
+            print((mean_elem.shape))
             
             dict_mean[elem] = mean_elem
             
@@ -131,7 +187,7 @@ def compute_mean_cormats(all_cormats,all_descriptors,descript_columns):
 
 def compute_stats_cormats(all_cormats,all_descriptors,descript_columns, groups = [], keep_intracon = False):
 
-    print(all_cormats.shape)
+    print((all_cormats.shape))
     
     for column in descript_columns:
     
@@ -153,8 +209,8 @@ def compute_stats_cormats(all_cormats,all_descriptors,descript_columns, groups =
         list_of_list_matrices = [all_cormats[all_descriptors[all_descriptors[column] == cond_name].index,:,:] for cond_name in groups]
         
         print(list_of_list_matrices)
-        print(len(list_of_list_matrices))
-        print(np.array(list_of_list_matrices).shape)
+        print((len(list_of_list_matrices)))
+        print((np.array(list_of_list_matrices).shape))
         
         
         signif_adj_mat, p_val_mat, F_stat_mat = compute_oneway_anova_fwe(list_of_list_matrices,cor_alpha = 0.05, uncor_alpha = 0.01, keep_intracon = keep_intracon)
@@ -171,7 +227,7 @@ def compute_stats_cormats(all_cormats,all_descriptors,descript_columns, groups =
             pair_name = "-".join(combi_pair)
             print(pair_name)
             
-            print(combi_pair[0])
+            print((combi_pair[0]))
             
             try:
                 signif_adj_mat,p_val_mat,T_stat_mat = compute_pairwise_ttest_fdr(X = list_of_list_matrices[groups.index(combi_pair[0])],
@@ -186,10 +242,10 @@ def compute_stats_cormats(all_cormats,all_descriptors,descript_columns, groups =
         
                 
             except AssertionError:
-                print("Stop running after {} was wrong".format(pair_name))
+                print(("Stop running after {} was wrong".format(pair_name)))
                 
         print(column)
-        print(list(dict_signif.keys()))
+        print((list(dict_signif.keys())))
         
     return dict_signif,dict_p_val,dict_stats
 
