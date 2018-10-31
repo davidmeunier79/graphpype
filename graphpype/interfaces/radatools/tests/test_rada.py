c72481a5cdbc16c8e049ce7996eec5cc5dfe22b9
David Meunier <david_meunier_79@hotmail.fr>
2018-10-31 11:09:05 +0100

    added test_net_prop_rada

--- graphpype/interfaces/radatools/tests/test_rada.py
+++ graphpype/interfaces/radatools/tests/test_rada.py
@@ -1,7 +1,8 @@
 """test rada"""
 import os
 
-from graphpype.interfaces.radatools.rada import (CommRada, PrepRada)
+from graphpype.interfaces.radatools.rada import (CommRada, PrepRada,
+                                                 NetPropRada)
 
 try:
     import neuropycon_data as nd
@@ -18,7 +19,7 @@ Pajek_net_file = os.path.join(data_path, "data_graph", "Z_List.net")
 
 
 def test_prep_rada():
-    """test prep_rada"""
+    """test PrepRada"""
     prep_rada = PrepRada()
     prep_rada.inputs.net_List_file = Z_list_file
     prep_rada.inputs.network_type = "U"
@@ -29,7 +30,7 @@ def test_prep_rada():
 
 
 def test_comm_rada():
-    """test comm_rada"""
+    """test CommRada"""
     comm_rada = CommRada()
     comm_rada.inputs.Pajek_net_file = Pajek_net_file
     comm_rada.inputs.optim_seq = "WS trfr 1"
@@ -43,3 +44,25 @@ def test_comm_rada():
     os.remove(val.rada_lol_file)
     os.remove(val.rada_log_file)
     os.remove(val.lol_log_file)
+
+
+def test_net_prop_rada():
+    """test NetPropRada"""
+    net_prop_rada = NetPropRada()
+    net_prop_rada.inputs.Pajek_net_file = Pajek_net_file
+
+    val = net_prop_rada.run().outputs
+
+    assert os.path.exists(val.global_file)
+    assert os.path.exists(val.dists_file)
+    assert os.path.exists(val.degrees_file)
+    assert os.path.exists(val.nodes_file)
+    assert os.path.exists(val.edges_betw_file)
+    assert os.path.exists(val.rada_log_file)
+
+    os.remove(val.global_file)
+    os.remove(val.dists_file)
+    os.remove(val.degrees_file)
+    os.remove(val.nodes_file)
+    os.remove(val.edges_betw_file)
+    os.remove(val.rada_log_file)
