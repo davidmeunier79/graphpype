df097dc30b0d1e1ef920f4401407ce1e1d3bfbb4
David Meunier <david_meunier_79@hotmail.fr>
2018-12-11 14:38:34 +0100

    modified test_utils_visbrain but do not work (seg fault)

--- graphpype/tests/test_utils_visbrain.py
+++ graphpype/tests/test_utils_visbrain.py
@@ -1,2 +1,50 @@
-# to be defined once OK
-pass
+#######################################################################################################################
+### test over visbrain plug-ins  ###
+#######################################################################################################################
+
+import os
+
+import numpy as np
+
+try:
+    import neuropycon_data
+except ImportError:
+    print ("neuropycon_data is not installed")
+
+import neuropycon_data as nd
+
+from graphpype.utils_visbrain import (visu_graph,visu_graph_modules)
+
+######### data
+
+data_path = os.path.join(nd.__path__[0], "data", "data_con")
+
+print(data_path)
+
+coords_file = os.path.join(data_path, "ROI_MNI_coords-Atlas.txt")
+
+labels_file = os.path.join(data_path, "ROI_labels-Atlas.txt")
+
+lol_file = os.path.join(data_path, "data_graph", "Z_List.lol")
+
+Pajek_net_file = os.path.join(data_path, "data_graph", "Z_List.net")
+
+######### tests
+
+def test_visu_graph_modules():
+    """
+    testing if visualisation of graph + module  is available
+    """
+    vb = visu_graph_modules(net_file=Pajek_net_file, lol_file=lol_file,
+                            coords_file=coords_file, labels_file=labels_file)
+    # TODO proper assert function
+
+def test_visu_graph():
+    """
+    testing if visualisation of graph is available
+    """
+    vb = visu_graph(net_file = Pajek_net_file, coords_file = coords_file,
+                    labels_file = labels_file)
+    # TODO proper assert function
+
+
