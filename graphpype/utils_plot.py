9c4bf5db5c862e73bb9fbe669c0c76839e4005d9
David Meunier <david.meunier@univ-amu.fr>
2025-10-10 11:08:35 +0200

    signals_matrix

--- graphpype/utils_plot.py
+++ graphpype/utils_plot.py
@@ -179,7 +179,7 @@ def plot_pair_signals(plot_signals_file, signal1, signal2, colors=[], labels=[],
 
     assert signal1.shape[0] == signal2.shape[0], (f"Signals should have the same length {signal1.shape[0]} =! {signal2.shape[0]}")
 
-    signals_matrix = np.vstack((signal1, signal2))
+    signals_matrix = np.vstack((signal1, signal2)).T
     print(signals_matrix.shape)
     0/0
 
