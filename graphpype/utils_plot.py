8d37fe6b52b78653eace0ff1eae972cbf0a09a35
David Meunier <david.meunier@univ-amu.fr>
2025-10-23 10:31:25 +0200

    bias

--- graphpype/utils_plot.py
+++ graphpype/utils_plot.py
@@ -161,19 +161,20 @@ def plot_sep_signals(plot_signals_file, signals_matrix, colors=[], labels=[],
     nb_signals = signals_matrix.shape[1]
     nb_timings = signals_matrix.shape[0]
 
-    bias = np.abs(np.amax(signals_matrix[:, 0]))
+    bias = 0
+    #bias = np.abs(np.amax(signals_matrix[:, 0]))
 
     biased_signals = [signals_matrix[:, 0] + bias]
 
-    bias += np.abs(np.amin(signals_matrix[:, 0]))
+    #bias += np.abs(np.amin(signals_matrix[:, 0]))
 
     for i in range(1, nb_signals):
-        print(i, bias)
-        bias +=  np.abs(np.min(signals_matrix[:, i]))
-        print(signals_matrix[:, i] + bias)
+        # print(i, bias)
+        # bias +=  np.abs(np.min(signals_matrix[:, i]))
+        # print(signals_matrix[:, i] + bias)
 
         biased_signals.append(signals_matrix[:, i] + bias)
-        bias +=  np.abs(np.max(signals_matrix[:, i]))
+        #bias +=  np.abs(np.max(signals_matrix[:, i]))
 
     biased_signals_matrix = np.array(biased_signals)
 
