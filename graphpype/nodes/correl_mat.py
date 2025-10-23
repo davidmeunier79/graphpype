255642b8f27a302a581b92319839b03383afc828
David Meunier <david.meunier@univ-amu.fr>
2025-10-23 10:35:22 +0200

    not norm

--- graphpype/nodes/correl_mat.py
+++ graphpype/nodes/correl_mat.py
@@ -1069,19 +1069,13 @@ class RegressCovar(BaseInterface):
 
         else:
             keep_regs = [a for a in regs if a is not None]
+
             rp = np.concatenate(keep_regs, axis=1)
-
-            norm_confounds = normalize_data(rp)
-
-            #  saving norm_confounds
-            norm_confounds_file = os.path.abspath('norm_confounds.npy')
-            np.save(norm_confounds_file, norm_confounds)
-
 
             if self.inputs.plot_fig:
 
-                plot_norm_confounds_file = os.path.abspath('norm_confounds.pdf')
-                plot_sep_signals(plot_norm_confounds_file, norm_confounds)
+                plot_confounds_file = os.path.abspath('confounds.pdf')
+                plot_signals(plot_confounds_file, rp)
 
 
             # regression movement parameters, return the residuals
